fec7f0732a9210b5cc06a62045e84f814db3256b
mrehmanfreed4472 <140436152+mrehmanfreed4472@users.noreply.github.com>
2025-06-09 21:54:47 +0500

    backend

--- config/db.js
+++ config/db.js
@@ -1,12 +1,14 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
+console.log('Loaded MONGO_URI:', process.env.MONGO_URI);
+
+
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI)
+    await mongoose.connect(process.env.MONGO_URI);
     console.log('MongoDB connected');
-  }
-   catch (error) {
+  } catch (error) {
     console.error('connection failed :', error);
     process.exit(1);
   }
